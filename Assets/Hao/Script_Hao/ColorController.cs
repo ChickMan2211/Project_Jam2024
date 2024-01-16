964e06b18f6093a74369b68c99ac49a203337126
ChickMan <120817243+ChickMan2211@users.noreply.github.com>
2024-01-16 14:26:34 +0700

    Xoay va di xuyen tuong

--- Assets/Hao/Script_Hao/ColorController.cs
+++ Assets/Hao/Script_Hao/ColorController.cs
@@ -13,6 +13,7 @@ public class ColorController : MonoBehaviour
 
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0)) // Kiểm tra khi người chơi nhấn chuột trái, để để test, có thể bỏ sau khi ok
         {
             RandomizeColors(); // Gọi hàm random màu 
@@ -21,18 +22,30 @@ public class ColorController : MonoBehaviour
 
     void RandomizeColors()
     {
-<<<<<<< Updated upstream:Assets/Script_Hao/ColorController.cs
-        foreach (Transform child in transform)
-=======
-        if (availableColors.Length != 0)
->>>>>>> Stashed changes:Assets/Hao/Script_Hao/ColorController.cs
+
+        if (availableColors != null)
         {
-            SpriteRenderer squareRenderer = child.GetComponent<SpriteRenderer>(); // Lấy reference tới SpriteRenderer của hình vuông
-            if (squareRenderer != null)
-            {
-                int randomIndex = Random.Range(0, availableColors.Length); // Chọn ngẫu nhiên index trong mảng màu
-                squareRenderer.color = availableColors[randomIndex]; // Thay đổi màu của hình vuông con
-            }
+            foreach (Transform child in transform)
+
+
+                foreach (Transform c in transform)
+                {
+                    if (availableColors.Length != 0)
+
+
+                    {
+                        SpriteRenderer squareRenderer = child.GetComponent<SpriteRenderer>(); // Lấy reference tới SpriteRenderer của hình vuông
+                        if (squareRenderer != null)
+                        {
+                            int randomIndex = Random.Range(0, availableColors.Length); // Chọn ngẫu nhiên index trong mảng màu
+                            squareRenderer.color = availableColors[randomIndex]; // Thay đổi màu của hình vuông con
+                        }
+                    }
+
+                }
+
+
+
         }
     }
 }
